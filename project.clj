19b6fef7f49abb52c616054382c442b13e89de1b
Christopher Miles <twitch@nervestaple.com>
2012-04-24 11:14:34 -0400

    Put a small, sample application together.

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject bishop-sample "1.0"
+  :description "A sample Bishop application"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [ring/ring-core "1.0.2"]
+                 [ring/ring-jetty-adapter "1.0.2"]
+                 [bishop "1.0-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure/swank-clojure "1.3.3"]]
+  :keep-non-project-classes true
+  :main com.tnrglobal.bishopsample.core)
