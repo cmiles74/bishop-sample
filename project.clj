acc36d1bfe74f340b83cec606702ed3b352c4bc6
Christopher Miles <twitch@nervestaple.com>
2012-05-06 11:56:01 -0400

    Updated the project to use the 1.0 release.

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [ring/ring-core "1.0.2"]
                  [ring/ring-jetty-adapter "1.0.2"]
-                 [bishop "1.0-SNAPSHOT"]
-                 [cheshire "4.0.0"]]
+                 [cheshire "4.0.0"]
+                 [tnrglobal/bishop "1.0"]]
   :dev-dependencies [[swank-clojure/swank-clojure "1.3.3"]]
   :keep-non-project-classes true
   :main com.tnrglobal.bishopsample.core)
